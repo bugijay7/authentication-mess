60b399cde256eec93b2491efc41ac42997ddcbdc
bugijay7 <bugijay7@gmail.com>
2025-05-10 14:12:36 +0300

    added styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import './home.css'; // Import your CSS file
 const Home = () => {
   const [user, setUser] = useState(null);
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'; // ✅ Import useNavigate
+import './register.css'; // ✅ Import your CSS file
 
 const Register = () => {
   const [username, setUsername] = useState('');
